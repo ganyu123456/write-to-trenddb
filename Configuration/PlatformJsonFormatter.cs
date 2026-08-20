d6fcd26aa5ee0f7c4d585d4d772c7a6f6079ccfd
ganyu123456 <1741478590@qq.com>
2026-08-20 10:56:44 +0800

    feat: 日志输出改为平台规范 JSON 格式（level 字段 + 大写枚举），适配日志中心级别识别

---
+++ Configuration/PlatformJsonFormatter.cs
@@ -0,0 +1,76 @@
+using System.Text;
+using Serilog.Events;
+using Serilog.Formatting;
+
+namespace WriteToTrendDb.Configuration;
+
+/// <summary>
+/// 平台日志规范的 JSON formatter。
+/// 输出字段：time / level（大写 TRACE|DEBUG|INFO|WARN|ERROR|FATAL）/ message。
+/// 对齐云边协同平台 Fluent Bit 的 json_log Parser 提取规则，
+/// 使日志级别能被日志中心正确识别并分流。
+/// </summary>
+public sealed class PlatformJsonFormatter : ITextFormatter
+{
+    /// <inheritdoc/>
+    public void Format(LogEvent logEvent, TextWriter output)
+    {
+        var level = logEvent.Level switch
+        {
+            LogEventLevel.Verbose => "TRACE",
+            LogEventLevel.Debug => "DEBUG",
+            LogEventLevel.Information => "INFO",
+            LogEventLevel.Warning => "WARN",
+            LogEventLevel.Error => "ERROR",
+            LogEventLevel.Fatal => "FATAL",
+            _ => "INFO",
+        };
+
+        var sb = new StringBuilder(256);
+        sb.Append("{\"time\":\"");
+        sb.Append(logEvent.Timestamp.ToString("yyyy-MM-ddTHH:mm:ss.fff"));
+        sb.Append("\",\"level\":\"");
+        sb.Append(level);
+        sb.Append("\",\"message\":\"");
+        sb.Append(JsonEscape(logEvent.RenderMessage()));
+        sb.Append('"');
+
+        if (logEvent.Exception != null)
+        {
+            sb.Append(",\"exception\":\"");
+            sb.Append(JsonEscape(logEvent.Exception.ToString()));
+            sb.Append('"');
+        }
+
+        sb.Append('}');
+        output.WriteLine(sb.ToString());
+    }
+
+    private static string JsonEscape(string s)
+    {
+        var sb = new StringBuilder(s.Length + 16);
+        foreach (var c in s)
+        {
+            switch (c)
+            {
+                case '"': sb.Append("\\\""); break;
+                case '\\': sb.Append("\\\\"); break;
+                case '\n': sb.Append("\\n"); break;
+                case '\r': sb.Append("\\r"); break;
+                case '\t': sb.Append("\\t"); break;
+                default:
+                    if (c < 0x20)
+                    {
+                        sb.Append("\\u");
+                        sb.Append(((int)c).ToString("x4"));
+                    }
+                    else
+                    {
+                        sb.Append(c);
+                    }
+                    break;
+            }
+        }
+        return sb.ToString();
+    }
+}
